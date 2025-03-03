e97ca26704a78ce96cb37d36e952d7c38ca84a41
Victor <160278295+VictoryMa8@users.noreply.github.com>
2025-03-03 14:17:48 -0600

    renaming

--- review/wrangling.py
+++ review/wrangling.py
@@ -3,10 +3,11 @@ import seaborn as sns
 import numpy as np
 
 iris = sns.load_dataset('iris')
+diamonds = sns.load_dataset('diamonds')
 
 def main():
     # sort ascending by sepal length
-    # print(iris.sort_values('sepal_length'))
+    print(iris.sort_values('sepal_length'))
 
     '''
     use .set_index() to have a column like 'id' to be the index
@@ -22,9 +23,7 @@ def main():
         ascending=False,
         inplace=True
         )
-
-    # print(iris)
-
+    
     # make a new DataFrame that is a copy of an existing dataframe, use copy() function
     # almost always want to say deep=True
     # this tells copy() to make a deep copy it will copy both the structure and values of the og data
@@ -32,28 +31,28 @@ def main():
     iris2 = iris2.sort_index()
 
     # see the data with only specified columns (a subset)
-    # print(iris2['species'])
+    print(iris2['species'])
 
     # multiple columns, surround columns with []
-    # print("Subset, species and petal_width: \n", iris2[['species', 'petal_width']])
+    print("Subset, species and petal_width: \n", iris2[['species', 'petal_width']])
 
     # iloc allows you to select subsets by row and column number
-    # print("Row 0: \n", iris2.iloc[0])
+    print("Row 0: \n", iris2.iloc[0])
 
     # give iloc two numbers, then it will select the value of the corresponding row and column
-    # print("Row 0, column 0: \n", iris2.iloc[0, 0])
+    print("Row 0, column 0: \n", iris2.iloc[0, 0])
 
     # you can also use slices with iloc (rows 1-2 (0:2), columns 4-5 (3:5))
-    # print("Slice: \n", iris2.iloc[0:2, 3:5])
+    print("Slice: \n", iris2.iloc[0:2, 3:5])
 
     # use loc (not iloc) to subset
     # loc function allows you to subset using the data frame's indexes
     # confusingly works differently than iloc:
-    # print(iris2.iloc[0:3])
-    # print(iris2.loc[0:3])
+    print(iris2.iloc[0:3])
+    print(iris2.loc[0:3])
 
     # loc makes more sense in my opinion
-    # print(iris2.loc[:, ['species', 'petal_width']])
+    print(iris2.loc[:, ['species', 'petal_width']])
 
     # subset by conditions (here, we print only virginica flowers)
     print(iris2[iris['petal_width'] >= 2.4])
@@ -111,6 +110,14 @@ def main():
     )
 
     print(big_virginicas)
+
+    # group by 'cut' (quality), look at mean price of each group
+    print(diamonds.groupby('cut')['price'].mean())
+
+    # look at max, min, size, etc.
+    print(diamonds.groupby('cut')['price'].max())
+    print(diamonds.groupby('cut')['price'].min())
+    print(diamonds.groupby('cut')['price'].size())
 
 if __name__ == "__main__":
     main()
