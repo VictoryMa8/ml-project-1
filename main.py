31b1a0838da57a03bfb65bb3399bcd2caff1a227
Victor <160278295+VictoryMa8@users.noreply.github.com>
2025-03-18 17:56:14 -0500

    anova tests

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import math
 import pandas as pd
-import numpy
+import numpy as np
+import pingouin as pg
+
+
 
 def euclidean(point1, point2):
   distance = math.sqrt((point1["x"] - point2["x"])**2 + (point1["y"] - point2["y"])**2)   # euclidean distance formula
@@ -14,11 +17,23 @@ def main():
   print(shoppers)
 
   sns.histplot(data = shoppers, x = 'VisitorType')
-  plt.show()
+  # plt.show()
 
   sns.scatterplot(data = shoppers, x = 'Administrative', y = 'Informational')
-  plt.show()
+  # plt.show()
 
+  print(shoppers['Revenue'].value_counts())
+  print(np.round(1908 / 10422, 4))
+
+  # two sample t-test
+  result1 = pg.ttest(shoppers['Administrative'], shoppers['Informational'], correction = True)
+  # print(result1)
+
+  # anova tests for visitor type
+  print(pg.anova(dv= 'BounceRates', between = 'VisitorType', data = shoppers))
+  print(pg.anova(dv = 'Administrative', between = 'VisitorType', data = shoppers))
+  print(pg.anova(dv= 'ExitRates', between = 'VisitorType', data = shoppers))
+  print(pg.anova(dv= 'PageValues', between = 'VisitorType', data = shoppers))
 
 if __name__ == "__main__":
     main()
